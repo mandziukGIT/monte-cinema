c6294e5adf09b45dfc4a14e13590886b50f3183e
Kacper Mandziuk <kacper.mandziuk@monterail.com>
2022-02-28 16:34:10 +0100

    adds config and assets, clear redundant code

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>
+      MonteCinema
+    </h1>
+    <p>
+      Best cinema out of this world!
+    </p>
+    <button>Book a ticket</button>
+    <router-link to='/'>See movies</router-link>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+}
+</script>
+
+<style lang="scss">
+  h1 {
+    color: $color-cherry-red;
+    font-family: "Eczar", serif;
+  }
+</style>
