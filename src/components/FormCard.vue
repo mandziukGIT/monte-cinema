4f856b1b3fff8c9dfc6800d956f1b478f6e2fb99
Kacper Mandziuk <kacper.mandziuk@monterail.com>
2022-03-03 19:41:53 +0100

    creates login form

---
+++ src/components/FormCard.vue
@@ -0,0 +1,25 @@
+<template>
+    <form class="form">
+        <div class="form__wrapper">
+            <slot name="form-controls">
+
+            </slot>
+            <slot name="form-actions">
+                        
+            </slot>
+        </div>
+    </form>
+</template>
+
+<style lang="scss" scoped>
+.form {
+    background-color: $color-snow-white;
+    box-shadow: 0px 4px 22px rgba(52, 53, 65, 0.15);
+    border-radius: 24px;
+    max-width: 600px;
+
+    &__wrapper {
+        padding: 3.5rem;
+    }
+}
+</style>
--- src/pages/public/account/LoginPage.vue
+++ src/pages/public/account/LoginPage.vue
@@ -1,3 +1,63 @@
 <template>
-    <h1>This is Login Page</h1>
+    <div class="container">
+        <h1 class="page-heading">Hi there! <br> <span>Care to log in?</span></h1>
+        <form-card>
+            <template #form-controls>
+                <label class="control-label">email</label>
+                <input type="text"/>
+                <label class="control-label">password</label>
+                <input type="password"/>
+            </template>
+            <template #form-actions>
+                <base-button>Register instead</base-button>
+                <base-button buttonType="primary">Log in</base-button>
+            </template>
+        </form-card>
+        <p class="info">Did you forget your password? <router-link to="">Reset it now</router-link></p>
+    </div>
 </template>
+
+<script>
+import FormCard from '@/components/FormCard.vue'
+export default {
+    components: {
+        FormCard
+    }
+}
+</script>
+<style lang="scss" scoped>
+
+.page-heading {
+    font-size: 80px;
+    span {
+        color: $color-jumbo;
+    }
+}
+.container {
+    font-family: 'Eczar', serif;
+    font-weight: 600;
+    max-width: 600px;
+    margin: 0 auto;
+    margin-top: 5rem;
+}
+    input {
+        width: 100%;
+        margin-bottom: 1rem;
+        margin-top: 0.5rem;
+    }
+    .control-label {
+        font-family: 'RobotoMono', monospace;
+        font-weight: 700;
+        color: $color-bittersweet;
+        text-transform: uppercase;
+        font-size: 14px;
+    }
+.info {
+    font-family: 'Roboto', monospace;
+    font-weight: 400;
+    margin-top: 3rem;
+    a {
+        color: $color-cherry-red;
+    }
+}
+</style>
