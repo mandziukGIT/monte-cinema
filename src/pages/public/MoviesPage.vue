8afddafdbaba73d127bfd2a4aac1528c4e8ea91a
Kacper Mandziuk <kacper.mandziuk@monterail.com>
2022-03-10 17:18:16 +0100

    removes unwanted code

--- src/pages/public/MoviesPage.vue
+++ src/pages/public/MoviesPage.vue
@@ -1,30 +1,3 @@
 <template>
-        <div class="movies__movies">
-            <MovieCard v-for="movie in movies" :key="movie.id" :movie="movie" />
-        </div>
+    <h1>This is MoviesPage</h1>
 </template>
-
-<script>
-import MovieCard from '@/components/movies/MovieCard.vue'
-export default {
-    components: {
-        MovieCard
-    },
-    computed: {
-        movies() {
-            return this.$store.getters.getMovies
-        }
-    }
-}
-</script>
-
-<style lang="scss" scoped>
-    .movies__wrapper {
-        display: grid;
-        grid-gap: 3em;  
-        grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
-        @include xs {
-            grid-template-columns: repeat(auto-fill, 1fr) 
-        }
-    }
-</style>
