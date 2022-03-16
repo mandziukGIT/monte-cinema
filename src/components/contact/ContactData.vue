a94d230b8cd12b72811173c99bbe7e91436968a3
Kacper Mandziuk <kacper.mandziuk@monterail.com>
2022-03-16 11:31:28 +0100

    adds contact section

---
+++ src/components/contact/ContactData.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="contact__wrapper">
+      <div class="contact__field">
+          <img src="@/assets/images/location-icon.svg" class="contact__field--icon">
+          <p class="contact__field--content">
+                Monterail Sp. z o.o. <br>
+                ul. Oławska 27-29 <br>
+                50-123 Wrocław <br>
+                Poland
+          </p>
+      </div>
+      <div class="contact__field">
+          <img src="@/assets/images/email-icon.svg" class="contact__field--icon">
+          <p class="contact__field--content">
+                hello@monterail.com
+          </p>
+      </div>
+      <div class="contact__field">
+          <img src="@/assets/images/phone-icon.svg" class="contact__field--icon">
+          <p class="contact__field--content">
+                +48 533 600 136
+          </p>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+    .contact {
+        &__wrapper {
+            max-width: 600px;
+            margin: 0 auto;
+            padding: 3rem;
+            border-radius: 24px;
+            background-color: $color-athens-gray;
+        }
+        &__field {
+            display: flex;
+            padding: 1rem 0;
+            line-height: 32px;
+            &--icon {
+                padding: 7px 20px 0 0;
+                align-self: flex-start;
+            }
+            &--content {
+                text-align: left;
+                font-family: "RobotoMono", monospace;
+                font-weight: 400;
+                font-size: 18px;
+                color: $color-tuna;
+                letter-spacing: 0.015em;
+            }
+        }
+    }
+</style>
--- src/pages/public/HomePage.vue
+++ src/pages/public/HomePage.vue
@@ -3,6 +3,7 @@
     <main-banner />
     <premieres-section />
     <screenings-section />
+    <contact-section />
   </div>
 </template>
 
@@ -10,9 +11,10 @@
 import MainBanner from '@/components/homepage/MainBanner.vue'
 import PremieresSection from '@/components/homepage/PremieresSection.vue'
 import ScreeningsSection from '@/components/screenings/ScreeningsSection.vue'
+import ContactSection from '@/components/homepage/ContactSection.vue'
 
 export default {
-  components: { MainBanner, PremieresSection, ScreeningsSection },
+  components: { MainBanner, PremieresSection, ScreeningsSection, ContactSection },
   name: 'HomePage',
 }
 </script>
