0e514bcb88446c169e7331def94d2c172ce4295f
Kacper Mandziuk <kacper.mandziuk@monterail.com>
2022-03-31 13:13:45 +0200

    adds contact page

--- src/components/contact/ContactData.vue
+++ src/components/contact/ContactData.vue
@@ -1,64 +1,60 @@
 <template>
   <div class="contact__wrapper">
-      <div class="contact__field">
-          <img src="@/assets/images/location-icon.svg" class="contact__field--icon">
-          <p class="contact__field--content">
-                Monterail Sp. z o.o. <br>
-                ul. Oławska 27-29 <br>
-                50-123 Wrocław <br>
-                Poland
-          </p>
-      </div>
-      <div class="contact__field">
-          <img src="@/assets/images/email-icon.svg" class="contact__field--icon">
-          <p class="contact__field--content">
-            <a href="mailto:hello@monterail.com">
-                hello@monterail.com
-            </a>
-          </p>
-      </div>
-      <div class="contact__field">
-          <img src="@/assets/images/phone-icon.svg" class="contact__field--icon">
-          <p class="contact__field--content">
-              <a href="tel:+48533-600-136">
-                  +48 533 600 136
-              </a>
-          </p>
-      </div>
+    <div class="contact__field">
+      <img
+        src="@/assets/images/location-icon.svg"
+        class="contact__field--icon"
+      />
+      <p class="contact__field--content">
+        Monterail Sp. z o.o. <br />
+        ul. Oławska 27-29 <br />
+        50-123 Wrocław <br />
+        Poland
+      </p>
+    </div>
+    <div class="contact__field">
+      <img src="@/assets/images/email-icon.svg" class="contact__field--icon" />
+      <p class="contact__field--content">
+        <a href="mailto:hello@monterail.com"> hello@monterail.com </a>
+      </p>
+    </div>
+    <div class="contact__field">
+      <img src="@/assets/images/phone-icon.svg" class="contact__field--icon" />
+      <p class="contact__field--content">
+        <a href="tel:+48533-600-136"> +48 533 600 136 </a>
+      </p>
+    </div>
   </div>
 </template>
 
-<script>
-export default {
-
-}
-</script>
-
 <style lang="scss" scoped>
-    .contact {
-        &__wrapper {
-            max-width: 600px;
-            margin: 0 auto;
-            padding: 3rem;
-            border-radius: 24px;
-            background-color: $color-athens-gray;
-        }
-        &__field {
-            display: flex;
-            padding: 1rem 0;
-            line-height: 32px;
-            &--icon {
-                padding: 7px 20px 0 0;
-                align-self: flex-start;
-            }
-            &--content {
-                text-align: left;
-                font-family: "RobotoMono", monospace;
-                font-weight: 400;
-                font-size: 18px;
-                color: $color-tuna;
-                letter-spacing: 0.015em;
-            }
-        }
+.contact {
+  &__wrapper {
+    padding: 3rem;
+    background-color: $color-athens-gray;
+    @include sm {
+      padding: 2rem;
     }
+    @include xs {
+      padding: 1.5rem;
+    }
+  }
+  &__field {
+    display: flex;
+    padding: 1rem 0;
+    line-height: 32px;
+    &--icon {
+      padding: 7px 20px 0 0;
+      align-self: flex-start;
+    }
+    &--content {
+      text-align: left;
+      font-family: "RobotoMono", monospace;
+      font-weight: 400;
+      font-size: 18px;
+      color: $color-tuna;
+      letter-spacing: 0.015em;
+    }
+  }
+}
 </style>
--- src/components/homepage/ContactSection.vue
+++ src/components/homepage/ContactSection.vue
@@ -1,26 +1,35 @@
 <template>
-    <div class="contact-section">
-        <h2 class="contact-section__headline">Contact us</h2>
-        <contact-data />
-    </div>
+  <div class="contact-section">
+    <h2 class="contact-section__headline">Contact us</h2>
+    <contact-data class="contact-section__card" />
+  </div>
 </template>
 
 <script>
-import ContactData from "@/components/contact/ContactData.vue"
+import ContactData from "@/components/contact/ContactData.vue";
 export default {
-    components: {
-        ContactData
-    }
-}
+  components: {
+    ContactData,
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-    .contact-section {
-        text-align: center;
-        &__headline{
-            font-family: "Eczar", serif;
-            font-weight: 600;
-            font-size: 64px
-        }
-    }
+.contact-section {
+  max-width: 600px;
+  margin: 0 auto;
+  text-align: center;
+  &__headline {
+    font-family: "Eczar", serif;
+    font-weight: 600;
+    font-size: 64px;
+  }
+  &__card {
+    border-radius: 24px;
+    overflow: hidden;
+  }
+  @include xs {
+    margin: 20px 2rem 20px 2rem;
+  }
+}
 </style>
